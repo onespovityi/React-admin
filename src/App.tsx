bed53654c38696dbd8c04f50cff4d4868a934180
Олег Несповитый <o.nespovitiy@gmail.com>
2023-12-19 15:24:24 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Admin, Resource, ShowGuesser } from "react-admin";
+import { dataProvider } from "./dataProvider";
+import { UserList } from "./users";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import PostIcon from "@mui/icons-material/Book";
+import UserIcon from "@mui/icons-material/Group";
+import { Dashboard } from "./Dashboard";
+import { authProvider } from "./authProvider";
+
+export const App = () => (
+  <Admin
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+    dashboard={Dashboard}
+  >
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource
+      name="users"
+      list={UserList}
+      show={ShowGuesser}
+      recordRepresentation="name"
+      icon={UserIcon}
+    />
+  </Admin>
+);
